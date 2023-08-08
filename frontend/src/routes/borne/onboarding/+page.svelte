a036b1170d7b4a4a3a2784a8fd0b2c74c05589bd
Yewolf <yyewolf@gmail.com>
2023-08-08 16:58:47 +0200

    feat(front): on board page

--- frontend/src/routes/borne/onboarding/+page.svelte
+++ frontend/src/routes/borne/onboarding/+page.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
-	import { fly, scale } from 'svelte/transition';
-	import QrOnBoarding from '$lib/components/borne/qr_onboarding.svelte';
+	import { fly } from 'svelte/transition';
+	import QrOnboarding from '$lib/components/borne/qr_onboarding.svelte';
+	import { onMount } from 'svelte';
+	import { elasticIn } from 'svelte/easing';
 
+	let shown = false;
 
+	onMount(() => {
+		setTimeout(() => {
+			shown = true;
+		}, 100);
+	});
 </script>
 
-<div class=
+{#if shown}
+	<div
+		class="grid lg:grid-cols-3 grid-cols-1 gap-16 p-16 w-full"
+		in:fly={{ x: -window.innerWidth, easing: elasticIn, duration: 1000, opacity: 1 }}
+	>
+		<QrOnboarding />
+	</div>
+{/if}
