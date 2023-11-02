93ae3fae478d5b72f13477927488c687fa62186c
Yewolf <yyewolf@gmail.com>
2023-11-02 16:25:27 +0100

    feat(porte): allow all cors

--- porte/main.go
+++ porte/main.go
@@ -7,6 +7,15 @@ import (
 func main() {
 	e := echo.New()
 
+	// CORS
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			origin := c.Request().Header.Get("Origin")
+			c.Response().Header().Set("Access-Control-Allow-Origin", origin)
+			return next(c)
+		}
+	})
+
 	routes(e)
 
 	e.Logger.Fatal(e.Start(":8080"))
