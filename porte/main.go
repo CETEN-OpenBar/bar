2782cb6359674163ac5b4133d518084acfa230c8
Yewolf <yyewolf@gmail.com>
2023-11-02 16:28:58 +0100

    fix(porte): cors

--- porte/main.go
+++ porte/main.go
@@ -12,6 +12,9 @@ func main() {
 		return func(c echo.Context) error {
 			origin := c.Request().Header.Get("Origin")
 			c.Response().Header().Set("Access-Control-Allow-Origin", origin)
+			c.Response().Header().Set("Access-Control-Allow-Credentials", "true")
+			c.Response().Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			c.Response().Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Local-Token")
 			return next(c)
 		}
 	})
