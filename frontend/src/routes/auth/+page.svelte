580ec3fd2a295b9e4ac287d336952c981857268e
Aristide Urli <aristide.urli@telecomnancy.net>
2024-09-11 22:52:32 +0200

    feat(frontend): Add bar logo to /auth page

--- frontend/src/routes/auth/+page.svelte
+++ frontend/src/routes/auth/+page.svelte
@@ -20,6 +20,12 @@
 
 <!-- Connect with Google at the middle of the screen -->
 <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="font-semibold text-3xl m-3">OpenBar</h1>
+    <img 
+        src="/logo.png"
+        alt="Logo du Bar"
+        class="h-48"
+    />
     <div class="flex flex-col items-center justify-center">
         <a href={api() + "/auth/google?r=client/commande"} class="connect-button mt-4">Connexion avec Google</a>
     </div>
