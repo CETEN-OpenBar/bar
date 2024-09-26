01263751fa3749070c07404d39955b4550e8a33e
Aristide Urli <aristide.urli@telecomnancy.net>
2024-09-27 00:13:23 +0200

    feat(frontend): Display breakpoints in test page

--- frontend/src/routes/test/+page.svelte
+++ frontend/src/routes/test/+page.svelte
@@ -1,1 +1,5 @@
-coucou
+<div class="sm:hidden">XS</div>
+<div class="hidden sm:block md:hidden">SM</div>
+<div class="hidden md:block lg:hidden">MD</div>
+<div class="hidden lg:block xl:hidden">LG</div>
+<div class="hidden xl:block">XL</div>
