c81fe5a955d4ad0b9feae95412cca316786ca625
JULLIEN Baptiste <baptiste.jullien@telecomnancy.eu>
2025-01-23 01:34:24 +0100

    feat(restock): change post resock and create patch restock

--- backend/api/restock.go
+++ backend/api/restock.go
@@ -23,7 +23,7 @@ func (s *Server) GetRestocks(c echo.Context, params autogen.GetRestocksParams) e
 		return nil
 	}
 
-	count, err := s.DBackend.CountAllRestocks(c.Request().Context())
+	count, err := s.DBackend.CountAllRestocks(c.Request().Context(), params.State)
 	if err != nil {
 		logrus.Error(err)
 		return Error500(c)
@@ -32,7 +32,7 @@ func (s *Server) GetRestocks(c echo.Context, params autogen.GetRestocksParams) e
 	// Make sure the last page is not empty
 	dbpage, page, limit, maxPage := autogen.Pager(params.Page, params.Limit, &count)
 
-	data, err := s.DBackend.GetAllRestocks(c.Request().Context(), dbpage, limit)
+	data, err := s.DBackend.GetAllRestocks(c.Request().Context(), dbpage, limit, params.State)
 	if err != nil {
 		logrus.Error(err)
 		return Error500(c)
@@ -72,6 +72,7 @@ func (s *Server) CreateRestock(c echo.Context) error {
 			TotalCostHt:   body.TotalCostHt,
 			TotalCostTtc:  body.TotalCostTtc,
 			Type:          body.Type,
+			State:         body.State,
 			CreatedAt:     uint64(time.Now().Unix()),
 			CreatedBy:     usr.Id,
 			CreatedByName: usr.Name(),
@@ -98,88 +99,32 @@ func (s *Server) CreateRestock(c echo.Context) error {
 				logrus.Error(err)
 				return nil, Error500(c)
 			}
-			category, err := s.DBackend.GetCategory(c.Request().Context(), item.CategoryId.String())
-			if err != nil {
-				if err == mongo.ErrNoDocuments {
-					// return nil, ErrorItemNotFound(c)
-					// Si l'item n'as pas de catégorie, on met une catégorie par défaut
-					category = &models.Category{
-						Category: autogen.Category{
-							Id:   uuid.Nil,
-							Name: "Autres",
-							SpecialPrice: false,
-						},
+			restockItem.ItemName = item.Name
+			restockItem.ItemPictureUri = item.PictureUri
+			if restock.State == autogen.RestockFinished {
+				category, err := s.DBackend.GetCategory(c.Request().Context(), item.CategoryId.String())
+				if err != nil {
+					if err == mongo.ErrNoDocuments {
+						// return nil, ErrorItemNotFound(c)
+						// Si l'item n'as pas de catégorie, on met une catégorie par défaut
+						category = &models.Category{
+							Category: autogen.Category{
+								Id:           uuid.Nil,
+								Name:         "Autres",
+								SpecialPrice: false,
+							},
+						}
+					} else {
+						logrus.Error(err)
+						return nil, Error500(c)
 					}
-				} else {
+				}
+				item = UpdateItem(item, category, restockItem)
+				err = s.DBackend.UpdateItem(c.Request().Context(), item)
+				if err != nil {
 					logrus.Error(err)
 					return nil, Error500(c)
 				}
-			}
-			restockItem.ItemName = item.Name
-			restockItem.ItemPictureUri = item.PictureUri
-			item.State = autogen.ItemBuyable
-			item.AmountLeft += restockItem.AmountOfBundle * restockItem.AmountPerBundle
-			item.LastTva = &restockItem.Tva
-			if category.SpecialPrice == false {
-				item.Prices.Coutant = uint64(math.Ceil(float64(restockItem.BundleCostTtc) / (float64(restockItem.AmountPerBundle))))
-				if item.Prices.Coutant < 30 {
-					item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant, 5) + 20
-					item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant, 5) + 10
-					item.Prices.StaffBar = arrondiAuMutilple(item.Prices.Coutant, 5) + 5
-					item.Prices.Privilegies = arrondiAuMutilple(item.Prices.Coutant, 5) + 5
-					item.Prices.Menu = arrondiAuMutilple(item.Prices.Coutant, 5) + 10
-				} else if item.Prices.Coutant >= 30 && item.Prices.Coutant < 130 {
-					item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant*3/2, 5)
-					item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant*113/100, 5)
-					item.Prices.StaffBar = arrondiAuMutilple(item.Prices.Coutant*108/100, 5)
-					item.Prices.Privilegies = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
-					item.Prices.Menu = arrondiAuMutilple(item.Prices.Coutant*13/10, 5)
-				} else if item.Prices.Coutant >= 130 && item.Prices.Coutant < 300 {
-					item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant*14/10, 5)
-					item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
-					item.Prices.StaffBar = arrondiAuMutilple(item.Prices.Coutant*108/100, 5)
-					item.Prices.Privilegies = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
-					item.Prices.Menu = arrondiAuMutilple(item.Prices.Coutant*12/10, 5)
-				} else if item.Prices.Coutant >= 300 {
-					item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant*125/100, 5)
-					item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
-					item.Prices.StaffBar = arrondiAuMutilple(item.Prices.Coutant*105/100, 5)
-					item.Prices.Privilegies = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
-					item.Prices.Menu = arrondiAuMutilple(item.Prices.Coutant*1125/1000, 5)
-				}
-			} else {
-				item.Prices.Coutant = uint64(math.Ceil(float64(restockItem.BundleCostTtc) / (float64(restockItem.AmountPerBundle))))
-				if item.Prices.Coutant < 30 {
-					item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant, 5) + 20
-					item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant, 5) + 10
-					item.Prices.StaffBar = item.Prices.Ceten
-					item.Prices.Privilegies = item.Prices.Ceten
-					item.Prices.Menu = item.Prices.Ceten
-				} else if item.Prices.Coutant >= 30 && item.Prices.Coutant < 130 {
-					item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant*3/2, 5)
-					item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant*113/100, 5)
-					item.Prices.StaffBar = item.Prices.Ceten
-					item.Prices.Privilegies = item.Prices.Ceten
-					item.Prices.Menu = item.Prices.Ceten
-				} else if item.Prices.Coutant >= 130 && item.Prices.Coutant < 300 {
-					item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant*14/10, 5)
-					item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
-					item.Prices.StaffBar = item.Prices.Ceten
-					item.Prices.Privilegies = item.Prices.Ceten
-					item.Prices.Menu = item.Prices.Ceten
-				} else if item.Prices.Coutant >= 300 {
-					item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant*125/100, 5)
-					item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
-					item.Prices.StaffBar = item.Prices.Ceten
-					item.Prices.Privilegies = item.Prices.Ceten
-					item.Prices.Menu = item.Prices.Ceten
-				}
-				item.Prices.Coutant = item.Prices.Ceten
-			}
-			err = s.DBackend.UpdateItem(c.Request().Context(), item)
-			if err != nil {
-				logrus.Error(err)
-				return nil, Error500(c)
 			}
 
 			restock.Items = append(restock.Items, restockItem)
@@ -206,6 +151,69 @@ func (s *Server) CreateRestock(c echo.Context) error {
 	return nil
 }
 
+func UpdateItem(item *models.Item, category *models.Category, restockItem autogen.RestockItem) *models.Item {
+	item.State = autogen.ItemBuyable
+	item.AmountLeft += restockItem.AmountOfBundle * restockItem.AmountPerBundle
+	item.LastTva = &restockItem.Tva
+	if !category.SpecialPrice {
+		item.Prices.Coutant = uint64(math.Ceil(float64(restockItem.BundleCostTtc) / (float64(restockItem.AmountPerBundle))))
+		if item.Prices.Coutant < 30 {
+			item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant, 5) + 20
+			item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant, 5) + 10
+			item.Prices.StaffBar = arrondiAuMutilple(item.Prices.Coutant, 5) + 5
+			item.Prices.Privilegies = arrondiAuMutilple(item.Prices.Coutant, 5) + 5
+			item.Prices.Menu = arrondiAuMutilple(item.Prices.Coutant, 5) + 10
+		} else if item.Prices.Coutant >= 30 && item.Prices.Coutant < 130 {
+			item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant*3/2, 5)
+			item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant*113/100, 5)
+			item.Prices.StaffBar = arrondiAuMutilple(item.Prices.Coutant*108/100, 5)
+			item.Prices.Privilegies = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
+			item.Prices.Menu = arrondiAuMutilple(item.Prices.Coutant*13/10, 5)
+		} else if item.Prices.Coutant >= 130 && item.Prices.Coutant < 300 {
+			item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant*14/10, 5)
+			item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
+			item.Prices.StaffBar = arrondiAuMutilple(item.Prices.Coutant*108/100, 5)
+			item.Prices.Privilegies = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
+			item.Prices.Menu = arrondiAuMutilple(item.Prices.Coutant*12/10, 5)
+		} else if item.Prices.Coutant >= 300 {
+			item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant*125/100, 5)
+			item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
+			item.Prices.StaffBar = arrondiAuMutilple(item.Prices.Coutant*105/100, 5)
+			item.Prices.Privilegies = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
+			item.Prices.Menu = arrondiAuMutilple(item.Prices.Coutant*1125/1000, 5)
+		}
+	} else {
+		item.Prices.Coutant = uint64(math.Ceil(float64(restockItem.BundleCostTtc) / (float64(restockItem.AmountPerBundle))))
+		if item.Prices.Coutant < 30 {
+			item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant, 5) + 20
+			item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant, 5) + 10
+			item.Prices.StaffBar = item.Prices.Ceten
+			item.Prices.Privilegies = item.Prices.Ceten
+			item.Prices.Menu = item.Prices.Ceten
+		} else if item.Prices.Coutant >= 30 && item.Prices.Coutant < 130 {
+			item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant*3/2, 5)
+			item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant*113/100, 5)
+			item.Prices.StaffBar = item.Prices.Ceten
+			item.Prices.Privilegies = item.Prices.Ceten
+			item.Prices.Menu = item.Prices.Ceten
+		} else if item.Prices.Coutant >= 130 && item.Prices.Coutant < 300 {
+			item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant*14/10, 5)
+			item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
+			item.Prices.StaffBar = item.Prices.Ceten
+			item.Prices.Privilegies = item.Prices.Ceten
+			item.Prices.Menu = item.Prices.Ceten
+		} else if item.Prices.Coutant >= 300 {
+			item.Prices.Externe = arrondiAuMutilple(item.Prices.Coutant*125/100, 5)
+			item.Prices.Ceten = arrondiAuMutilple(item.Prices.Coutant*11/10, 5)
+			item.Prices.StaffBar = item.Prices.Ceten
+			item.Prices.Privilegies = item.Prices.Ceten
+			item.Prices.Menu = item.Prices.Ceten
+		}
+		item.Prices.Coutant = item.Prices.Ceten
+	}
+	return item
+}
+
 // (DELETE /restocks/{restock_id})
 func (s *Server) DeleteRestock(c echo.Context, restockId autogen.UUID) error {
 	account, err := MustGetAdmin(c)
@@ -224,22 +232,24 @@ func (s *Server) DeleteRestock(c echo.Context, restockId autogen.UUID) error {
 			return nil, Error500(c)
 		}
 
-		for _, item := range restock.Items {
-			i, err := s.DBackend.GetItem(c.Request().Context(), item.ItemId.String())
-			if err != nil {
-				if err == mongo.ErrNoDocuments {
-					return nil, ErrorItemNotFound(c)
+		if restock.State == autogen.RestockFinished {
+			for _, item := range restock.Items {
+				i, err := s.DBackend.GetItem(c.Request().Context(), item.ItemId.String())
+				if err != nil {
+					if err == mongo.ErrNoDocuments {
+						return nil, ErrorItemNotFound(c)
+					}
+					logrus.Error(err)
+					return nil, Error500(c)
 				}
-				logrus.Error(err)
-				return nil, Error500(c)
-			}
 
-			i.AmountLeft -= item.AmountOfBundle * item.AmountPerBundle
+				i.AmountLeft -= item.AmountOfBundle * item.AmountPerBundle
 
-			err = s.DBackend.UpdateItem(c.Request().Context(), i)
-			if err != nil {
-				logrus.Error(err)
-				return nil, Error500(c)
+				err = s.DBackend.UpdateItem(c.Request().Context(), i)
+				if err != nil {
+					logrus.Error(err)
+					return nil, Error500(c)
+				}
 			}
 		}
 
---
+++ backend/api/validerRestock.go
@@ -0,0 +1,89 @@
+package api
+
+import (
+	"bar/autogen"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+// PATCH /restocks/{restock_id}
+func (s *Server) UpdateRestock(c echo.Context, restockId autogen.UUID) error {
+	usr, err := MustGetAdmin(c)
+	if err != nil {
+		return nil
+	}
+
+	var body autogen.UpdateRestockJSONRequestBody
+	if err := c.Bind(&body); err != nil {
+		logrus.Error(err)
+		return Error400(c)
+	}
+
+	restock, err := s.DBackend.GetRestock(c.Request().Context(), restockId.String())
+	if err != nil {
+		logrus.Error(err)
+		return Error500(c)
+	}
+
+	restock.Items = []autogen.RestockItem{}
+	restock.TotalCostHt = body.TotalCostHt
+	restock.TotalCostTtc = body.TotalCostTtc
+	restock.Type = body.Type
+	restock.CreatedAt = uint64(time.Now().Unix())
+
+	oldState := restock.State
+	if restock.State != autogen.RestockFinished {
+		restock.State = body.State // Cannot go from finished to anything else
+	}
+
+	for _, newRestockItem := range body.Items {
+		restockItem := autogen.RestockItem{
+			AmountOfBundle:  newRestockItem.AmountOfBundle,
+			AmountPerBundle: newRestockItem.AmountPerBundle,
+			BundleCostHt:    newRestockItem.BundleCostHt,
+			BundleCostTtc:   newRestockItem.BundleCostTtc,
+			ItemId:          newRestockItem.ItemId,
+			Tva:             newRestockItem.Tva,
+			ItemName:        newRestockItem.ItemName,
+		}
+
+		item, err := s.DBackend.GetItem(c.Request().Context(), restockItem.ItemId.String())
+		if err != nil {
+			logrus.Error(err)
+			return Error500(c)
+		}
+		restockItem.ItemPictureUri = item.PictureUri
+
+		restock.Items = append(restock.Items, restockItem)
+
+		if oldState != autogen.RestockFinished && body.State == autogen.RestockFinished {
+
+			category, err := s.DBackend.GetCategory(c.Request().Context(), item.CategoryId.String())
+			if err != nil {
+				logrus.Error(err)
+				return Error500(c)
+			}
+			item = UpdateItem(item, category, restockItem)
+			err = s.DBackend.UpdateItem(c.Request().Context(), item)
+			if err != nil {
+				logrus.Error(err)
+				return Error500(c)
+			}
+			logrus.WithField("restock", restock.Id.String()).WithField("by", usr.Name()).Info("Items updated")
+		}
+	}
+	err = s.DBackend.UpdateRestock(c.Request().Context(), restock)
+	if err != nil {
+		logrus.Error(err)
+		return Error500(c)
+	}
+
+	if oldState == body.State {
+		return nil
+	}
+
+	logrus.WithField("restock", restock.Id.String()).WithField("by", usr.Name()).Info("Patch restock")
+	return nil
+}
