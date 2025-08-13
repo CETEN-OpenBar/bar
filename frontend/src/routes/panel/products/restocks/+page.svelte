e7c465ffd10e5c05f7dcf3d236c31c202f23d2a5
Hugo Dartoy <hugo.dartoy@telecomnancy.eu>
2025-08-13 22:50:40 +0200

    fix(invoice-parser): empty unknown items when new restock

--- frontend/src/routes/panel/products/restocks/+page.svelte
+++ frontend/src/routes/panel/products/restocks/+page.svelte
@@ -497,6 +497,7 @@
 								type="submit"
 								class="mt-4 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
 								on:click={() => {
+									unknownItems = [];
 									let items;
 									if (newRestock.type == "auchan") {
 										items = restockAuchan(pdfText);										
