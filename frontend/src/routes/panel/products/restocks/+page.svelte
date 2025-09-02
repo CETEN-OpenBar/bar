d7b93fa2f2da8c2f53ba1d5b414b897452bda687
Hugo Dartoy <hugo.dartoy@telecomnancy.eu>
2025-09-02 19:44:36 +0200

    fix(invoice-parser): adapt parser to new Auchan Drive invoices

--- frontend/src/routes/panel/products/restocks/+page.svelte
+++ frontend/src/routes/panel/products/restocks/+page.svelte
@@ -287,42 +287,64 @@
     }
   };
 
-	function restockAuchanDrive(input: string): Array<{ reference: number; name: string; quantity: number; unitPriceHT: number; tvaRate: number }> {	
-		const splitPage = "Référence   Caractéristiques produit   Qte   Prix U. (HT) € Remise U. (HT) € Prix Total (HT) € Taux TVA Cagnotte WAAOH! Prix Total TTC €  ";
-		const splitLine = "   "
+	function isDigit(chr: string): boolean{
+		return chr >= '0' && chr <= '9';
+	}
+
+	function isPrice(str: string): boolean{
+		const len = str.length;
+		return len >= 4 && isDigit(str[len-1]) && isDigit(str[len-2]) && str[len-3] == ',' && isDigit(str[len-4]);
+	}
+
+	function restockAuchanDrive(input: string): Array<{ reference: number; name: string; quantity: number; unitPriceHT: number; tvaRate: number }> {
+		const splitPage = "Référence   Caractéristiques produit   Prix U. (HT) € Remises U. (HT) €   Qte.   Prix total (HT) € Taux TVA % Cagnotte WAAOH! Prix total (TTC) €  "
+		const splitLine = "   ";
 		const endPage1 = "Votre commande :";
-		const endPage2 = "Taux TVA";		
+		const endPage2 = "Taux TVA";	
+		const endPage3 = "Votre facture :";
 		const pages = input.split(splitPage);
 		const itemsList: Array<{ reference: number; name: string; quantity: number; unitPriceHT: number; tvaRate: number }> = [];
 		for (let i=1;i<pages.length;i++){
 			const pageSplit = pages[i].split(splitLine);
-
-			for (let j = 0; j < pageSplit.length; j += 7) {				
-				if (pageSplit[j].includes(endPage1) || pageSplit[j].includes(endPage2)) {				
+			for (let j = 0; j < pageSplit.length; ) {
+				if (pageSplit[j].includes(endPage1) || pageSplit[j].includes(endPage2) || pageSplit[j].includes(endPage3)) {				
 					break;
 				}
 				const reference = parseInt(pageSplit[j].split(" ").slice(-1)[0]);
+				j++;
 				let name;
-				let quantity;
-				if (pageSplit[j+1].slice(-1) >= '0' && pageSplit[j+1].slice(-1) <= '9'){
-					name = pageSplit[j+1].split(' ').slice(0,-1).join(" ");
-					quantity = parseInt(pageSplit[j+1].split(' ').slice(-1)[0]);
+				let unitPriceHT;
+				let splitName = pageSplit[j].split(" ");
+				if (isPrice(splitName[splitName.length-1])){
+					name = splitName.slice(0, -1).join(" ");
+					unitPriceHT = parseFloat(splitName[splitName.length-1].replace(",","."));
 				}
 				else{
-					name = pageSplit[j+1];
+					name = pageSplit[j];
 					j++;
-					quantity = parseInt(pageSplit[j+1]);
-				}							
-				const unitPriceHT =  parseFloat(pageSplit[j+2].replace(',','.')) - parseFloat(pageSplit[j+3].replace(',','.'));
-				const tvaRate = parseFloat(pageSplit[j+5].replace(',','.'));
+					unitPriceHT = parseFloat(pageSplit[j].replace(",","."));
+				}
+				j++;
+				if (isPrice(pageSplit[j])){ // Check if there is a discount
+					unitPriceHT -= parseFloat(pageSplit[j].replace(",","."));
+					j++;
+				}
+				const quantity = parseInt(pageSplit[j]);
+				j+=2;
+				const tvaRate = parseFloat(pageSplit[j].replace(",","."));
+				j++;
+				if (isPrice(pageSplit[j])){ // Check if there is a cagnotte WHAOOH					
+					j++;
+				}				
 				itemsList.push({
 					reference,
 					name,
 					quantity,
-					unitPriceHT,					
-					tvaRate					
+					unitPriceHT,
+					tvaRate
 				});
-			}			
+
+			}
 		}
 		return itemsList;
 	}
