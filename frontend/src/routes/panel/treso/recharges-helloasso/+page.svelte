95d886303098c5349ed5d9eb685a614696adc182
Aristide Urli <aristide.urli@telecomnancy.net>
2025-09-10 20:49:50 -0400

    feat: add remote refill validation in treso pannel

--- frontend/src/routes/panel/treso/recharges-helloasso/+page.svelte
+++ frontend/src/routes/panel/treso/recharges-helloasso/+page.svelte
@@ -4,6 +4,16 @@
     import { RemoteRefillState } from '$lib/api';
 	import { refillsApi } from '$lib/requests/requests';
 	import 'iconify-icon';
+	import Spinner from '$lib/components/spinner.svelte';
+	import Success from '$lib/components/success.svelte';
+	import Error from '$lib/components/error.svelte';
+	import { goto } from '$app/navigation';
+
+    const state_names: Record<RemoteRefillState, string> = {
+        [RemoteRefillState.RemoteRefillStarted]: "En attente",
+        [RemoteRefillState.RemoteRefillProcessed]: "Validé",
+        [RemoteRefillState.RemoteRefillAbandoned]: "Abandonné",
+    };
 
 	let refills: RemoteRefill[] = [];
 	let start_date = '';
@@ -43,11 +53,61 @@
 		refills_per_page = data.limit;
 	}
 
+    let loading = false;
+    let error: string | undefined = undefined
+    let success: boolean = false
+
+    const verifyRefill = (id: string) => {
+        loading = true;
+        refillsApi().verifyRemoteRefill(id, {withCredentials: true})
+        .then((resp) => {
+            if (resp.status != 200) {
+                throw resp;
+            }
+            success = true
+            fetchRefills();
+        })
+        .catch((err) => {
+            switch (err.status) {
+                case 401:
+                    goto("/auth");
+                    break;
+                case 403:
+                    error = "Vous n'êtes pas autorisé à faire ceci";
+                    break;
+                case 404:
+                    error = "Rechargement introuvable";
+                    fetchRefills();
+                    break;
+                case 402:
+                    error = "Paiement non validé par HelloAsso";
+                    break;
+                case 409:
+                    error = "Ce rechargement a déjà été validé";
+                    fetchRefills();
+                    break;
+                case 500:
+                default:
+                    error = "Une erreur est survenue, réessayez plus tard"
+            }
+        })
+        .finally(() => {
+            loading = false;
+            setTimeout(() => {error = undefined; success = false}, 2000);
+        })
+    }
+
 	onMount(fetchRefills);
 
-    console.log(Object.entries(RemoteRefillState))
-
 </script>
+
+{#if success}
+<Success message = "Rechargement validé !" close={() => {success = false}}/>
+{/if}
+
+{#if error}
+<Error {error} close={() => {error = undefined}}/>
+{/if}
 
 <div class="flex-grow grid grid-cols-1 grid-rows-[auto_1fr_auto] bg-gray-50 dark:bg-gray-900 min-w-full">
 	<div class="m-3 p-2">
@@ -96,70 +156,13 @@
                 >
                     <option value={undefined}></option>
                     {#each Object.values(RemoteRefillState) as state}
-                        <option value={state}>{state}</option>
+                        <option value={state}>{state_names[state]}</option>
                     {/each}
                 </select>
             </div>
 		</div>
 	</div>
 
-    <!--
-	<div class="flex-grow w-full overflow-x-auto">
-		<table class="min-w-full bg-white dark:bg-gray-800 rounded-lg shadow-sm">
-			<thead class="grid grid-cols-[1fr_1.5fr_1fr_0.75fr_0.5fr_0.5fr] bg-gray-50 dark:bg-gray-700 divide-x divide-gray-200 dark:divide-gray-700" >
-				<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
-                    Date
-                </th>
-				<th	class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
-                    Compte
-                </th>
-				<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"> 
-                    Montant
-                </th>
-				<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider opacity-60"> 
-                    État
-                </th>
-                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider opacity-60"> 
-                    Action
-                </th>
-			</thead>
-			<tbody class="divide-y divide-gray-200 dark:divide-gray-700">
-				{#each refills as refill}
-					<tr class="grid grid-cols-[1fr_1.5fr_1fr_0.75fr_0.5fr_0.5fr] divide-x divide-gray-200 dark:divide-gray-700">
-						<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-300">
-                            {new Date(refill.created_at * 1000).toLocaleDateString('fr-FR', {
-								day: 'numeric',
-								month: 'long',
-								year: 'numeric',
-								hour: '2-digit',
-								minute: '2-digit'
-							})}
-                        </td>
-						<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-300">
-                            {refill.account_name}
-                        </td>
-						<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-300">
-                            {refill.amount} €
-						</td>
-						<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-300 opacity-60">
-                            {refill.state}
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-300">
-                            {#if refill.state == RemoteRefillState.RemoteRefillAbandoned || refill.state == RemoteRefillState.RemoteRefillStarted}
-                                <button
-                                    class="rounded-sm bg-green-500 hover:bg-green-600 text-white dark:text-white p-2 text-left flex items-center gap-2"
-                                >
-                                    <iconify-icon icon="mdi:refresh" width="20" height="20" />
-                                    <div class="align-middle">Vérifier</div>
-                                </button>
-                            {/if}
-                        </td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-	</div>
-    -->
     <div class="flex-grow w-full overflow-x-auto min-w-full">
         <table class="w-full min-w-full bg-white dark:bg-gray-800 rounded-lg shadow-sm">
             <thead class="bg-gray-50 dark:bg-gray-700">
@@ -197,16 +200,25 @@
                     {refill.account_name}
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-300">
-                    {refill.amount} €
+                    {refill.amount / 100 } €
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-300 opacity-60">
-                    {refill.state}
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-300">
+                    {state_names[refill.state]}
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-300">
                     {#if refill.state == RemoteRefillState.RemoteRefillAbandoned || refill.state == RemoteRefillState.RemoteRefillStarted}
-                    <button class="rounded-sm bg-green-500 hover:bg-green-600 text-white dark:text-white p-2 text-left flex items-center gap-2">
+                    <button 
+                        class="relative rounded-sm bg-green-500 hover:bg-green-600 disabled:hover:bg-green-500 text-white dark:text-white p-2 text-left flex items-center gap-2"
+                        on:click={() => {verifyRefill(refill.id)}}
+                        disabled={loading}
+                    >
                         <iconify-icon icon="mdi:refresh" width="20" height="20" />
                         <div class="align-middle">Vérifier</div>
+                        {#if loading}
+                        <div class="absolute w-full h-full top-0 left-0 flex justify-center items-center bg-gray-600 bg-opacity-50 rounded-sm">
+                            <Spinner size="23px"/>
+                        </div>
+                        {/if}
                     </button>
                     {/if}
                 </td>
